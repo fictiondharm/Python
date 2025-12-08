d9ba6621839a753d62d7776583e843f3f7696894
fictiondharm <151057980+fictiondharm@users.noreply.github.com>
2025-12-08 18:51:45 +0530

    Add files via upload
    day 1 update

---
+++ main.py
@@ -0,0 +1,46 @@
+# TODAY IS DAY OF CODING
+print(" today is day to code ")
+print("i will start to cde every day also i have sem exam lets eee how it goes")
+
+#vairables
+date = "7/12/2025"
+print(f" today is he first day of coding and the date is {date}")
+
+#data types
+# string 
+my_self = " dharmendar"
+print(f"so my name is {my_self} and i am here to code ")
+
+#float
+ruppes = 15.50
+print(f"i have totally {ruppes} rupess with me ")
+
+#boolean 
+
+is_stduent = True
+print(f" was that a blur car : {is_stduent}?")
+
+#type casting
+
+name = 'dharmendar'
+age = 20
+gpa = 7.2
+is_stduent = True
+
+print(type(is_stduent))
+
+#type casting age to float
+
+age = float(age)
+print(age)
+
+#input fucntion
+
+name = input("what is your name :")
+
+print(f"my name is {name}")
+
+age = input("what is your age")
+age = int(age)
+age = age + 1
+print(f"my age is {age}")
